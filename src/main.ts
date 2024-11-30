0cad0dcca930b05449494099b46e412a1f180f50
Ahmed Hany <ah7608867@gmail.com>
2024-11-30 18:27:10 +0200

    Body valiator and DTO

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
-
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(new ValidationPipe()); // for validation, it will automatically validate the incoming data
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
-
+import { CreateMessageDto } from './dtos/create-message.dto';
 @Controller('messages')
 export class MessagesController {
     @Get()
@@ -14,8 +14,9 @@ export class MessagesController {
     }
 
     @Post()
-    createMessage(@Body() body: any) {
+    createMessage(@Body() body: CreateMessageDto) {
         console.log('body: ', body);
+        console.log('content: ', body.content);
         return 'Create a message';
     }
 
